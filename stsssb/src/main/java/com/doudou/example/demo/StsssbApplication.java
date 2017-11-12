a0f4735438174c604ce02907739870592bc9fb1f
eggsoup <wujg1991@163.com>
2017-11-12 14:26:41 +0800

    创建ssb项目

---
+++ stsssb/src/main/java/com/doudou/example/demo/StsssbApplication.java
@@ -0,0 +1,12 @@
+package com.doudou.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StsssbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StsssbApplication.class, args);
+	}
+}
---
+++ stsssb/src/test/java/com/doudou/example/demo/StsssbApplicationTests.java
@@ -0,0 +1,16 @@
+package com.doudou.example.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StsssbApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
